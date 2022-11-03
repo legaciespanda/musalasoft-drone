88ae309b7695ef53dc3340ba5d4bd8c60ee6ffc9
Ernest Obot <youandinews@gmail.com>
2022-11-03 13:06:37 +0100

    Drone and Medication DTO created

---
+++ src/main/java/com/musalasoft/drone/dto/DroneDto.java
@@ -0,0 +1,22 @@
+package com.musalasoft.drone.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.*;
+import java.io.Serializable;
+
+@Data
+public class DroneDto implements Serializable {
+//    private final Long droneId;
+    @NotNull
+    @Min(value = 100) //drone serial must not be more than 100 charcters
+    private final String serial;
+    @NotNull
+    private final String model;
+    @NotNull
+    private final int weightLimit;
+    @NotNull
+    private final int batteryCapacity;
+    @NotNull
+    private final String state;
+}
