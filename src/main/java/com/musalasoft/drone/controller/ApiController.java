8ab3935eb918ac248289f16db506d6f58ec72487
Ernest Obot <youandinews@gmail.com>
2022-11-03 14:42:40 +0100

    checking available drones for loading and check drone battery level for a given drone implemented

--- src/main/java/com/musalasoft/drone/controller/ApiController.java
+++ src/main/java/com/musalasoft/drone/controller/ApiController.java
@@ -2,6 +2,7 @@ package com.musalasoft.drone.controller;
 
 import com.musalasoft.drone.dto.DroneDto;
 import com.musalasoft.drone.entity.Drone;
+import com.musalasoft.drone.response.ResponseHandler;
 import com.musalasoft.drone.service.DroneService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +12,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/api/v1")
 public class ApiController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(ApiController.class);
@@ -20,9 +26,10 @@ public class ApiController {
     @Autowired
     DroneService droneService;
 
-    @CrossOrigin(origins = "*")
+
+    //registering a drone
     @RequestMapping(path = "/musalasoft/drone", method = RequestMethod.POST)
-    private ResponseEntity<String> registerDrone(@Valid @RequestBody DroneDto droneDto){
+    private ResponseEntity<Object> registerDrone(@Valid @RequestBody DroneDto droneDto){
 
         //drone states must be in uppercase
         String droneState = droneDto.getState().toUpperCase();
@@ -34,12 +41,50 @@ public class ApiController {
         drone.setBatteryCapacity(droneDto.getBatteryCapacity());
         drone.setState(droneState);
 
-       LOGGER.info("Drone Resgistered" + drone);
+       LOGGER.info("Drone Registered" + drone);
         try {
             droneService.registerDrone(drone);
-            return new ResponseEntity<>("Drone Registered \n "+drone, HttpStatus.CREATED);
+            return ResponseHandler.generateResponse("Drone Registered Successfully!", HttpStatus.OK, drone);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
+    }
+
+    //checking available drones for loading;
+    @RequestMapping(path = "/musalasoft/drone/loading", method = RequestMethod.GET)
+    private ResponseEntity<Object> loadingDrones(){
+
+        List<Drone> drones = new ArrayList<>();
+
+        try {
+            //getting all drones with the loading status
+                droneService.getLoadingDrones("LOADING").forEach(drones::add);
+            return ResponseHandler.generateResponse("Available drones for loading", HttpStatus.OK, drones);
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
+    }
+
+    //checking available drones for loading;
+    @RequestMapping(path = "/musalasoft/drone/battery-level", method = RequestMethod.GET)
+    private ResponseEntity<Object> droneBatteryLevel(@RequestParam(required = false, name = "serial") String serial){
+        //checking available drones for loading using their drone serial number
+        Optional<Drone> drone = droneService.droneBatteryLevel(serial);
+
+        try {
+            //check if drone with serial number exist
+//            if(drone.isPresent()){
+//                String batteryCapacity = String.valueOf(drone.get().getBatteryCapacity() + "%");
+//                return ResponseHandler.generateResponse("Drone battery level", HttpStatus.OK, batteryCapacity);
+//            }else {
+//                return ResponseHandler.generateResponse("Unable to find drone with serial number or drone does not exist", HttpStatus.NOT_FOUND, null);
+//            }
+
+            String batteryCapacity = String.valueOf(drone.get().getBatteryCapacity() + "%");
+            return ResponseHandler.generateResponse("Drone battery level", HttpStatus.OK, batteryCapacity);
+
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, drone);
         }
     }
 
