c18f4a8d2388e021013eb047df642499ebf6cfb5
Ernest Obot <youandinews@gmail.com>
2022-11-03 13:11:31 +0100

    custom response exception implemented

---
+++ src/main/java/com/musalasoft/drone/advice/CustomResponseEntityException.java
@@ -0,0 +1,21 @@
+package com.musalasoft.drone.advice;
+
+import com.musalasoft.drone.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+
+@ControllerAdvice
+public class CustomResponseEntityException {
+    @ExceptionHandler
+    public ResponseEntity<ApiResponse> handle(ConstraintViolationException constraintViolationException){
+        String errorMessage = new ArrayList<>(constraintViolationException.getConstraintViolations()).get(0).getMessage();
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setResponseMessage(errorMessage);
+        return new ResponseEntity<>(apiResponse, null, HttpStatus.BAD_REQUEST);
+    }
+}
